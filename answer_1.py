b8dee0efc26f7e730f1f279a79081e8aa55be15a
ronsamgeorge <77411064+ronsamgeorge@users.noreply.github.com>
2021-03-15 12:01:24 +0530

    update

--- answer_1.py
+++ answer_1.py
@@ -1,5 +1,3 @@
-import datetime 
-
 def server_cost(d1, m1, y1, d2, m2, y2):
     cost = 0
     
@@ -8,16 +6,14 @@ def server_cost(d1, m1, y1, d2, m2, y2):
         cost = 20000
     elif (d2 == d1) and (m2 == m1) and (y2==y1): # if on the same day
         cost = 20 
-    elif(d2>= d1)  and ( (m2 > m1) or (y2 > y1)): #if greater than a month y2 >y1 check because it can also get carried to different year in which case m2 will be less than m1
+    elif ( (d2>=d1 and m2==m1+1) or (m2 > m1+1) or (y2 > y1)): #if greater than a month ; y2 >y1 check because it can also get carried to different year in which case m2 will be less than m1
         if y2>y1 :
             m2 += 12
         cost = 1000 * (m2-m1)
-
-    elif (m2<= m1+1):
+    elif (m2<= m1+1): # if the days are less then a month , and m1+1 is kept in case 30 days isnt completed in the given month itself, highest it can reach is next month 
         if m2==(m1+1):
             d2 += 30
         cost = 30 * (d2-d1)
-
 
     return cost
 
@@ -26,7 +22,6 @@ if __name__ == '__main__':
     d1 = int(d1M1Y1[0])
     m1 = int(d1M1Y1[1])
     y1 = int(d1M1Y1[2])
-
     d2M2Y2 = input().split()
     d2 = int(d2M2Y2[0])
     m2 = int(d2M2Y2[1])
